e6e9ec016edb78171d711361c200adfe25e8d8e6
Poros Enjoyers <138520882+PorosEnyoyers@users.noreply.github.com>
2025-12-09 13:35:04 -0800

    1

--- Chapter 20/Iterators Algorithms.cpp
+++ Chapter 20/Iterators Algorithms.cpp
@@ -1,35 +1,164 @@
 #include <iostream>
-#include <iterator>
-#include <utility>
 
+//void print(int x)
+//{
+//    std::cout << "print(int)\n";
+//}
+//
+//void print(int x, int y = 10)
+//{
+//    std::cout << "print(int, int)\n" << y;
+//}
+//
 //int main()
 //{
-//	int array[]{ 30, 50, 20, 10, 40 };
-//	constexpr int length{ static_cast<int>(std::size(array)) };
+//    static_cast<void(*)(int)>(print)(1);
 //
-//	for (int startIndex{ 0 }; startIndex < length - 1; ++startIndex)
-//	{
-//		int smallestIndex{ startIndex };
+//    static_cast<void(*)(int, int)>(print)(1,0);
 //
-//		for (int currentIndex{ startIndex + 1 }; currentIndex < length; ++currentIndex)
-//		{
-//			if (array[currentIndex] < array[smallestIndex])
-//				smallestIndex = currentIndex;
-//		}
-//
-//		std::swap(array[startIndex], array[smallestIndex]);
-//	}
-//
-//	for (int index{ 0 }; index < length; ++index)
-//		std::cout << array[index] << ' ';
-//
-//	std::cout << '\n';
-//
-//	return 0;
+//    return 0;
 //}
+
+#include <iostream>
+#include <limits>
+#include <functional>
+
+class Data
+{
+private:
+	double m_x{ 1.0 };
+	double m_y{ 1.0 };
+	char m_z{ '*' };
+public:
+	double getFirstNum() { return m_x; }
+	void setFirstNum(double x) { m_x = x; }
+
+	double getSecondNum() { return m_y; }
+	void setSecondNum(double y) { m_y = y; }
+
+	char getOperation() { return m_z; }
+	void setOperation(char z) { m_z = z; }
+};
+
+using ValidateFunction = std::function<double(Data&)>;
+
+void ignoreLine()
+{
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+bool clearFailedExtraction()
+{
+	if (!std::cin)
+	{
+		if (std::cin.eof())
+		{
+			std::exit(0);
+		}
+
+		std::cin.clear();
+		return true;
+	}
+
+	return false;
+}
+
+bool hasUnextractedInput()
+{
+	return ((!std::cin.eof()) && (std::cin.peek() != '\n'));
+}
+
+void getUserInputs(Data& x)
+{
+	double temp{};
+	char holder{};
+	while (true)
+	{
+		std::cout << "Enter your first integer: ";
+		std::cin >> temp;
+		if (clearFailedExtraction() || hasUnextractedInput())
+		{
+			ignoreLine();
+			std::cout << "Invalid inputs!!! Please Retry!!!\n";
+			continue;
+		}
+		x.setFirstNum(temp);
+		break;
+	}
+
+	while (true)
+	{
+		std::cout << "Enter your second integer: ";
+		std::cin >> temp;
+		if (clearFailedExtraction() || hasUnextractedInput())
+		{
+			ignoreLine();
+			std::cout << "Invalid inputs!!! Please Retry!!!\n";
+			continue;
+		}
+		x.setSecondNum(temp);
+		break;
+	}
+
+	while (true)
+	{
+		std::cout << "Enter your mathetical operation (+, -, *, /): ";
+		std::cin >> holder;
+		if (clearFailedExtraction() || hasUnextractedInput() || (holder != '*' && holder != '-' && holder != '/' && holder != '+'))
+		{
+			ignoreLine();
+			std::cout << "Invalid inputs!!! Please Retry!!!\n";
+			continue;
+		}
+		x.setOperation(holder);
+		break;
+	}
+}
+
+using ValidateFunction = std::function<double(Data&)>;
+
+double add(Data& x)
+{
+	return x.getFirstNum() + x.getSecondNum();
+}
+
+double sub(Data& x)
+{
+	return x.getFirstNum() - x.getSecondNum();
+}
+
+double mult(Data& x)
+{
+	return x.getFirstNum() * x.getSecondNum();
+}
+
+double divi(Data& x)
+{
+	return x.getFirstNum() / x.getSecondNum();
+}
+
+ValidateFunction getArithmeticFunction(Data& x)
+{
+	switch (x.getOperation())
+	{
+	case '+':
+		return &add;
+	case '-':
+		return &sub;
+	case '*':
+		return &mult;
+	case '/':
+		return &divi;
+	default:
+		return nullptr;
+	}
+}
 
 int main()
 {
-	int array[]{ 30,60,20,50,40,10 };
-	constexpr int length {static_cast}
+	Data x{};
+	getUserInputs(x);
+	ValidateFunction y{ getArithmeticFunction(x) };
+	std::cout << x.getFirstNum() << ' ' << x.getOperation() << ' ' << x.getSecondNum() << " = " << y(x) << '\n';
+	return 0;
 }
